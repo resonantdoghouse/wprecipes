1c4c957d682fe9cecb57ff51849858b34af28260
Jim <jim@advancebass.com>
2017-11-26 23:22:31 -0800

    added footer nav styles

--- themes/wprecipes/footer.php
+++ themes/wprecipes/footer.php
@@ -1,14 +1,16 @@
 <footer class="wprecipes__footer">
 
-	<?php
-	wp_nav_menu( array(
-		'menu'           => 'WP Recipes Nav',
-		'theme_location' => 'footer'
-	) );
-	?>
+    <div class="wprecipes__footer__nav">
+		<?php
+		wp_nav_menu( array(
+			'menu'           => 'WP Recipes Nav',
+			'theme_location' => 'footer'
+		) );
+		?>
+    </div>
 
     <div class="wprecipes__info">
-        <span><?php echo get_bloginfo('name'); ?></span>
+        <span><?php echo get_bloginfo( 'name' ); ?></span>
         <time><?php echo date( 'Y' ); ?></time>
     </div>
 </footer>
