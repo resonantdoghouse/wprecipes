6c34635f7aaf14fcffa0f43034db45e91463ebf8
Jim <jim@advancebass.com>
2017-11-28 21:47:52 -0800

    added more loops, post navigation, refactor and tidy

--- themes/wprecipes/front-page.php
+++ themes/wprecipes/front-page.php
@@ -37,27 +37,77 @@ get_header(); ?>
                 <h1><?php _e( 'Posts', 'wprecipes' ); ?></h1>
                 <div class="wprecipes__owlcarousel">
                     <div class="owl-carousel owl-theme">
-				        <?
-				        /**
-				         * Recipe Posts Loop
-				         */
-				        if ( have_posts() ):
-					        $args = array( 'posts_per_page' => 6, 'post' => 'recipe' );
-					        $myposts = get_posts( $args );
-					        foreach ( $myposts as $post ) : setup_postdata( $post ); ?>
+						<?
+						/**
+						 * Recipe Posts Loop
+						 */
+						if ( have_posts() ):
+							$args = array( 'posts_per_page' => 6, 'post' => 'recipe' );
+							$myposts = get_posts( $args );
+							foreach ( $myposts as $post ) : setup_postdata( $post ); ?>
                                 <article class="wprecipes__main__post">
                                     <a href="<?php echo get_the_permalink(); ?>">
                                         <h1 class="wprecipes__page-title"><?php echo esc_html( get_the_title() ); ?></h1>
-								        <?php if ( has_post_thumbnail() ):
-									        the_post_thumbnail( 'wprecipe-thumb' );
-								        endif; ?>
+										<?php if ( has_post_thumbnail() ):
+											the_post_thumbnail( 'wprecipe-thumb' );
+										endif; ?>
                                     </a>
                                 </article>
-					        <?php endforeach;
-					        wp_reset_postdata();
-				        endif; ?>
+							<?php endforeach;
+							wp_reset_postdata();
+						endif; ?>
                     </div>
                 </div>
+            </section>
+
+            <section>
+                <h1><?php _e( 'More Recipes', 'wprecipes' ); ?></h1>
+				<?php
+				$args_recipes = array(
+					'post_type'      => 'recipe',
+					'post_status'    => 'publish',
+					'posts_per_page' => 3,
+					'orderby'        => 'rand',
+					'paged'          => true
+				);
+				$the_query    = new WP_Query( $args_recipes );
+				?>
+
+				<?php
+				if ( $the_query->have_posts() ) { ?>
+                    <ul class="wprecipes__list">
+						<?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+                            <li class="wprecipes__list__item">
+                                <a href="<?php the_permalink(); ?>"><?php echo get_the_title(); ?></a>
+                            </li>
+						<?php endwhile; ?>
+                    </ul>
+
+                    <div class="wprecipes__postnav__wrapper">
+						<?php
+						the_post_navigation( array(
+							'prev_text'          => __( 'prev recipe: %title' ),
+							'next_text'          => __( 'next recipe: %title' ),
+							'in_same_term'       => false,
+							'taxonomy'           => __( 'recipe-type' ),
+							'screen_reader_text' => __( 'Continue Reading' ),
+						) );
+						?>
+                    </div>
+
+					<?php
+
+
+					/* Restore original Post Data */
+					wp_reset_postdata();
+
+
+				} else {
+					// no posts found
+				}
+
+
+				?>
             </section>
 
         </main>
--- themes/wprecipes/single-recipe.php
+++ themes/wprecipes/single-recipe.php
@@ -3,79 +3,110 @@
  * Front Page WP Recipes
  */
 get_header(); ?>
+
     <div class="wprecipes__site">
         <main class="wprecipes__main">
-			<? if ( have_posts() ):
-				while ( have_posts() ): the_post(); ?>
-                    <article class="wprecipes__main__post">
-                        <h1 class="wprecipes__page-title"><?php echo esc_html( get_the_title() ); ?></h1>
-						<?php if ( has_post_thumbnail() ):
-							the_post_thumbnail( 'large' );
-						endif; ?>
-						<?php the_content();
-						/**
-						 * ACF repeater
-						 */
-						if ( have_rows( 'wprecipes_recipe_ingredients' ) ): ?>
-                            <section id="wprecipes-recipe" class="wprecipes__recipe">
-                                <button id="wprecipes-increase-recipe">&plus;</button>
-                                <button id="wprecipes-decrease-recipe">&minus;</button>
-                                <ul class="wprecipes__recipe__items">
-									<?php while ( have_rows( 'wprecipes_recipe_ingredients' ) ) : the_row(); ?>
-                                        <li class="wprecipes__recipe__items__item">
+
+			<? if ( have_posts() ): while ( have_posts() ): the_post(); ?>
+
+                <article class="wprecipes__main__post">
+                    <h1 class="wprecipes__page-title"><?php echo esc_html( get_the_title() ); ?></h1>
+
+					<?php if ( has_post_thumbnail() ):
+						the_post_thumbnail( 'large' );
+					endif;
+
+					the_content();
+
+					/**
+					 * ACF repeater
+					 */
+					if ( have_rows( 'wprecipes_recipe_ingredients' ) ): ?>
+                        <section id="wprecipes-recipe" class="wprecipes__recipe">
+                            <button id="wprecipes-increase-recipe">&plus;</button>
+                            <button id="wprecipes-decrease-recipe">&minus;</button>
+                            <ul class="wprecipes__recipe__items">
+								<?php while ( have_rows( 'wprecipes_recipe_ingredients' ) ) : the_row(); ?>
+                                    <li class="wprecipes__recipe__items__item">
 											<span class="wprecipes__recipe__items__item__quantity">
                                                 <?php the_sub_field( 'wprecipes_recipe_ingredient_quantity' ); ?>
                                             </span>
-											<?php the_sub_field( 'wprecipes_recipe_ingredient_quantity_measurement' ); ?>
-                                            <strong><?php the_sub_field( 'wprecipes_recipe_ingredient' ); ?></strong>
-                                            <input class="ingredient-checked" type="checkbox">
-                                        </li>
-									<?php endwhile; ?>
-                                </ul>
-                                <ul>
-									<?php
-									/**
-									 * Taxonomy Terms
-									 */
-									$recipe_term_list = wp_get_post_terms( $post->ID, 'recipe-type',
-										array( "fields" => "all" ) );
+										<?php the_sub_field( 'wprecipes_recipe_ingredient_quantity_measurement' ); ?>
+                                        <strong><?php the_sub_field( 'wprecipes_recipe_ingredient' ); ?></strong>
+                                        <input class="ingredient-checked" type="checkbox">
+                                    </li>
+								<?php endwhile; ?>
+                            </ul>
 
-									foreach ( $recipe_term_list as $recipe_term ): ?>
-                                        <li>
-                                            <a href="<?php echo get_term_link( $recipe_term ); ?>">
-												<?php echo $recipe_term->name; ?>
-                                            </a>
-                                        </li>
-									<?php endforeach; ?>
-                                </ul>
-                            </section>
-						<?php else :
-							// no rows found
-						endif; ?>
-                    </article>
-				<?php endwhile;
-			endif; ?>
-            <h2><?php _e( 'Related Products', 'wprecipes' ); ?></h2>
-			<?php
-			$recipe_related_products_key = 'wprecipes_related_products';
-			$recipe_related_products_obj = get_field_object( $recipe_related_products_key );
-			$recipe_related_products     = get_field( $recipe_related_products_key );
-			if ( $recipe_related_products ): ?>
-                <ul class="wprecipes__related-products">
-					<?php foreach ( $recipe_related_products as $post ): ?>
-						<?php setup_postdata( $post ); ?>
-                        <li class="wprecipes__related-products__item">
-                            <a href="<?php the_permalink(); ?>">
-								<?php the_title( '<h3>', '</h3>' ); ?>
-								<?php if ( has_post_thumbnail() ):
-									the_post_thumbnail( 'thumbnail' );
-								endif; ?>
-                            </a>
-                        </li>
-					<?php endforeach; ?>
-                </ul>
-				<?php wp_reset_postdata(); ?>
-			<?php endif; ?>
+                            <ul>
+								<?php
+								/**
+								 * Taxonomy Terms
+								 */
+								$recipe_term_list = wp_get_post_terms( $post->ID, 'recipe-type',
+									array( "fields" => "all" ) );
+
+								foreach ( $recipe_term_list as $recipe_term ): ?>
+                                    <li>
+                                        <a href="<?php echo get_term_link( $recipe_term ); ?>">
+											<?php echo $recipe_term->name; ?>
+                                        </a>
+                                    </li>
+								<?php endforeach; ?>
+                            </ul>
+
+                        </section>
+
+					<?php else : // no rows found
+					endif; ?>
+
+                </article>
+
+			<?php endwhile; // have_posts ?>
+
+                <div class="wprecipes__postnav__wrapper">
+					<?php
+					the_post_navigation( array(
+						'prev_text'          => __( 'prev recipe: %title' ),
+						'next_text'          => __( 'next recipe: %title' ),
+						'in_same_term'       => false,
+						'taxonomy'           => __( 'recipe-type' ),
+						'screen_reader_text' => __( 'Continue Reading' ),
+					) );
+					?>
+                </div>
+
+			<?php endif; // have_posts ?>
+
+            <section id="related-products">
+                <h1><?php _e( 'Related Products', 'wprecipes' ); ?></h1>
+
+				<?php
+				$recipe_related_products_key = 'wprecipes_related_products';
+				$recipe_related_products_obj = get_field_object( $recipe_related_products_key );
+				$recipe_related_products     = get_field( $recipe_related_products_key );
+
+				if ( $recipe_related_products ): ?>
+                    <ul class="wprecipes__related-products">
+						<?php foreach ( $recipe_related_products as $post ): ?>
+							<?php setup_postdata( $post ); ?>
+                            <li class="wprecipes__related-products__item">
+                                <a href="<?php the_permalink(); ?>">
+									<?php the_title( '<h3>', '</h3>' ); ?>
+									<?php if ( has_post_thumbnail() ):
+										the_post_thumbnail( 'thumbnail' );
+									endif; ?>
+                                </a>
+                            </li>
+							<?php
+
+						endforeach; ?>
+                    </ul>
+
+					<?php wp_reset_postdata(); ?>
+				<?php endif; ?>
+            </section>
+
         </main>
 		<? get_sidebar(); ?>
     </div>
